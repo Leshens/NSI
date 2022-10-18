0d7ff659e4910872a61b6ce208f68b0d1d471476
Kacper Kamm <75252260+Leshens@users.noreply.github.com>
2022-10-18 19:04:28 +0200

    lab2 end

--- LAB2/zad2.cpp
+++ LAB2/zad2.cpp
@@ -9,25 +9,6 @@
 
 using mojafunkcja_t = std::function<double(std::vector<double>)>;
 
-std::vector<double> get_result(std::function<double(std::vector<double>)> f, double border_1, double border_2, int iterations){
-    std::random_device rd; // obtain a random number from hardware
-    std::mt19937 gen(rd()); // seed the generator
-    std::uniform_real_distribution<double> distr(border_1, border_2);
-    std::vector<double> closest_numbers = {distr(gen), distr(gen)};
-
-    double result = f(closest_numbers);
-    for(int u = 0; u < iterations; u++){
-        std::vector<double> args = {distr(gen), distr(gen)};
-        double new_result = f(args);
-        if (new_result < result){
-            result = new_result;
-            closest_numbers = args;
-        }
-
-    }
-    return closest_numbers;
-}
-
 int main(int argc, char **argv) {
     using namespace std;
     map<string, mojafunkcja_t> optfunction;
@@ -54,28 +35,11 @@ int main(int argc, char **argv) {
         auto selected_f = argumenty.at(1);
         auto f = optfunction.at(selected_f);
         auto c = f(values);
-        //cout << "result is: " << c << endl;
+        cout << "result is: " << c << endl;
     }
     catch (std::out_of_range aor) {
         cout << "Enter a valid argument. The available are: ";
         for (auto [k, v] : optfunction) cout << " " << k;
         cout << endl;
     }
-    vector<string> argumenty(argv, argv + argc);
-    auto selected_f = argumenty.at(1);
-    auto f = optfunction.at(selected_f);
-
-        std::vector<std::string> argument(argv, argv + argc);
-        std::string func_name = argument.at(1);
-
-        std::vector<double> values;
-        transform(argv + 2, argv + argc,  std::back_inserter(values), [](const char* v){return std::stod(v);});
-        double border_low = values.at(0);
-        double border_high = values.at(1);
-        std::vector<double> my_result = get_result(f, -10.0, 10.0, 10000000);
-        std::cout << my_result[0] << std::endl;
-        std::cout << my_result[1] << std::endl;
-
-
-    return 0;
 }
