e53c00d0934d7aa197ca512afee063d0b49388be
Kacper Kamm <75252260+Leshens@users.noreply.github.com>
2022-12-14 10:11:35 +0100

    lab8

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,40 +1,107 @@
-#include<iostream>
-#include<opencv2/highgui/highgui.hpp>
-#include<opencv2/imgproc/imgproc.hpp>
-using namespace std;
-using namespace cv;
-int main(int argc, char** argv) {
-    VideoCapture video_load(0);//capturing video from default camera//
-    namedWindow("Adjust");//declaring window to show the image//
-    int Hue_Lower_Value = 27;//initial hue value(lower)//
-    int Hue_Lower_Upper_Value = 101;//initial hue value(upper)//
-    int Saturation_Lower_Value = 65;//initial saturation(lower)//
-    int Saturation_Upper_Value = 175;//initial saturation(upper)//
-    int Value_Lower = 91;//initial value (lower)//
-    int Value_Upper = 202;//initial saturation(upper)//
-    createTrackbar("Hue_Lower", "Adjust", &Hue_Lower_Value, 179);//track-bar for lower hue//
-    createTrackbar("Hue_Upper", "Adjust", &Hue_Lower_Upper_Value, 179);//track-bar for lower-upper hue//
-    createTrackbar("Sat_Lower", "Adjust", &Saturation_Lower_Value, 255);//track-bar for lower saturation//
-    createTrackbar("Sat_Upper", "Adjust", &Saturation_Upper_Value, 255);//track-bar for higher saturation//
-    createTrackbar("Val_Lower", "Adjust", &Value_Lower, 255);//track-bar for lower value//
-    createTrackbar("Val_Upper", "Adjust", &Value_Upper, 255);//track-bar for upper value//
-    while (1) {
-        Mat actual_Image;//matrix to load actual image//
-        bool temp = video_load.read(actual_Image);//loading actual image to matrix from video stream//
-        Mat convert_to_HSV;//declaring a matrix to store converted image//
-        cvtColor(actual_Image, convert_to_HSV, COLOR_BGR2HSV);//converting BGR image to HSV and storing it in convert_to_HSV matrix//
-        Mat detection_screen;//declaring matrix for window where object will be detected//
-        inRange(convert_to_HSV,Scalar(Hue_Lower_Value,Saturation_Lower_Value, Value_Lower),Scalar(Hue_Lower_Upper_Value,Saturation_Upper_Value, Value_Upper), detection_screen);//applying track-bar modified value of track-bar//
-        erode(detection_screen, detection_screen, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));//morphological opening for removing small objects from foreground//
-        dilate(detection_screen, detection_screen, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));//morphological opening for removing small object from foreground//
-        dilate(detection_screen, detection_screen, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));//morphological closing for filling up small holes in foreground//
-        erode(detection_screen, detection_screen, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));//morphological closing for filling up small holes in foreground//
-        imshow("Threesholded Image", detection_screen);//showing detected object//
-        imshow("Original", actual_Image);//showing actual image//
-        if (waitKey(30) == 27){ //if esc is press break the loop//
-            break;
-        }
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+
+
+bool compareContourAreas ( std::vector<cv::Point> contour1, std::vector<cv::Point> contour2 ) {
+    double i = fabs( contourArea(cv::Mat(contour1)) );
+    double j = fabs( contourArea(cv::Mat(contour2)) );
+    return ( i < j );
+}
+
+int main( int argc, char** argv ) {
+    bool capturing = true;
+    cv::VideoCapture cap;
+
+    int deviceID = 0;             // 0 = open default camera
+    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+    // open selected camera using selected API
+    cap.open(deviceID, apiID);
+    if ( !cap.isOpened() ) {
+        std::cerr << "error opening frames source" << std::endl;
+        return -1;
     }
+    std::cout << "Video size: " << cap.get( cv::CAP_PROP_FRAME_WIDTH )
+    << "x" << cap.get( cv::CAP_PROP_FRAME_HEIGHT ) << std::endl;
+    namedWindow("detected", cv::WINDOW_AUTOSIZE);
+/*
+ HL=21
+ HU=35
+ SL=128
+ SU=45
+ VL=138
+ VU=208
+ */
+    std::vector<int> lower = {21,128,138};
+    std::vector<int> upper = {35,45,200};
+    cv::createTrackbar("HL", "detected", &lower[0], 255);
+    cv::createTrackbar("SL", "detected", &lower[1], 255);
+    cv::createTrackbar("VL", "detected", &lower[2], 255);
+    cv::createTrackbar("HU", "detected", &upper[0], 255);
+    cv::createTrackbar("SU", "detected", &upper[1], 255);
+    cv::createTrackbar("VU", "detected", &upper[2], 255);
+    do {
+        cv::Mat frame;
+        if ( cap.read( frame ) ) {
+            cv::flip(frame,frame,1);
+
+            cv::Mat gray_image;
+            GaussianBlur(frame, frame, cv::Size(3, 3), 0);
+            cvtColor( frame, gray_image, cv::COLOR_BGR2HSV );
+            cv::Mat imgThresholded;
+
+//gfg 0 ,141, 87 - 15, 255, 255
+            inRange(gray_image, cv::Scalar(lower[0], lower[1], lower[2]), cv::Scalar(upper[0], upper[1], upper[2]), imgThresholded);
+            auto kernel = getStructuringElement(cv::MORPH_ELLIPSE,cv::Size{5,5});
+
+            cv::Mat eroded;
+            dilate(imgThresholded, eroded, kernel);
+//            erode(eroded, eroded, kernel);
+
+//            cv::Mat canny_output;
+//            Canny( eroded, canny_output, 10, 255 );
+
+            std::vector<std::vector<cv::Point> > contours;
+            std::vector<cv::Vec4i> hierarchy;
+            findContours(eroded, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+
+            std::sort(contours.begin(), contours.end(), compareContourAreas);
+            if (contours.size() >= 2) {
+                std::vector<cv::Point> BiggestContour = contours[contours.size() - 1];
+                std::vector<cv::Point> SecondBiggestContour = contours[contours.size() - 2];
+                auto m = moments(BiggestContour, false);
+                cv::Point p1 = {(int) (m.m10 / m.m00), (int) (m.m01 / m.m00)};
+                int x = (int) (m.m10 / m.m00);
+                int y = (int) (m.m01 / m.m00);
+
+
+                auto m2 = moments(SecondBiggestContour, false);
+                int y2 = (int) (m2.m01 / m2.m00);
+                cv::Point p2 = {(int) (m2.m10 / m2.m00), (int) (m2.m01 / m2.m00)};
+
+
+                if (abs(y2 - y) < 40) {
+                    line(frame, p1, p2, cv::Scalar(255, 0, 0),
+                         10, cv::LINE_8);
+                }
+            }
+
+
+
+            cv::imshow("zakresy", eroded);
+            cv::imshow("flipped",frame);
+
+
+
+        } else {
+            // stream finished
+            capturing = false;
+        }
+        //czekaj na klawisz, sprawdz czy to jest 'esc'
+        if( (cv::waitKey( 1000.0/60.0 )&0x0ff) == 27 ) capturing = false;
+    } while( capturing );
     return 0;
 }
 /*
